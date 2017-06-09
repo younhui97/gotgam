1c0b6e664caa3a0c6ed55d0cb75d515c4db9fceb
yh <ssalborry@gmail.com>
2017-06-09 16:39:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yh/gotgam/NextActivity.java
@@ -0,0 +1,18 @@
+package com.example.yh.gotgam;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by yh on 2017. 6. 9..
+ */
+
+public class NextActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
