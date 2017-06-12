2db834e181e78464468491565b62f9330e1e1409
yh <ssalborry@gmail.com>
2017-06-12 21:36:02 +0900

    170612 21:35

--- app/src/main/java/com/example/yh/gotgam/NextActivity.java
+++ app/src/main/java/com/example/yh/gotgam/NextActivity.java
@@ -1,8 +1,11 @@
 package com.example.yh.gotgam;
 
 import android.app.Activity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by yh on 2017. 6. 9..
@@ -15,4 +18,17 @@ public class NextActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
     }
+
+    public Context mContext;
+    public void sendMessageTo(String phoneNum, String message) {
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNum, null, message, null, null);
+
+        Toast.makeText(mContext, "Message transmission is completed.",
+                Toast.LENGTH_SHORT).show();
+    }public void onClick02(View v) {
+        sendMessageTo("01012345678", "This is a test message.");
+    }
+
 }
