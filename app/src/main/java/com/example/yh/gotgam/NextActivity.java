0b4f47e261df020978e1c8a8127e87efc60b06fc
yh <ssalborry@gmail.com>
2017-06-12 22:14:41 +0900

    170612 22:14

--- app/src/main/java/com/example/yh/gotgam/NextActivity.java
+++ app/src/main/java/com/example/yh/gotgam/NextActivity.java
@@ -1,34 +1,28 @@
 package com.example.yh.gotgam;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.Toast;
 
 /**
  * Created by yh on 2017. 6. 9..
  */
 
-public class NextActivity extends Activity{
+public class NextActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
     }
 
-    public Context mContext;
-    public void sendMessageTo(String phoneNum, String message) {
+    public void onClick02(View v)
+    {
+        Intent intent = new Intent( Intent.ACTION_VIEW,Uri.parse( "sms:01087319379" ));
+        startActivity(intent);
 
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(phoneNum, null, message, null, null);
-
-        Toast.makeText(mContext, "Message transmission is completed.",
-                Toast.LENGTH_SHORT).show();
-    }public void onClick02(View v) {
-        sendMessageTo("01012345678", "This is a test message.");
     }
+
 
 }
